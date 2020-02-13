4a7e907808a398900df3e7d9c36c340e4b34aa33
mjolsic <wongqianjie1234@gmail.com>
2020-02-13 23:45:45 +0800

    Created page navigate button
    Page navigate button partially works.

--- js/information_page.js
+++ js/information_page.js
@@ -1,27 +1,21 @@
 
-function infoPage(information){
-  let KEY = 'INDEX';
-  console.log(information)
-  if (localStorage[KEY] !== undefined){
-    let index = localStorage.getItem(KEY);
-    let selectedObj = '';
-    information.forEach((item,i) => {
-      if (information[i].Name === index){
-        selectedObj = information[i];
-      }
-    });
-    console.log(selectedObj)
-    let name1 = selectedObj.Name;
-    let name2 = selectedObj.Name_2;
-    let year = selectedObj.Year
-    let picture = getElement('id','picture')
-    picture.src = 'images/done/' + convertInput(name1,'image') + '.png';
-    let title = getElement('id','title');
-    title.innerText = name1 + '   ' + name2 + '(' + year + ')';
-    let details = getElement('id','details');
-    details.innerText = selectedObj.Introduction;
-    let player = getElement('id','player');
-    let link = 'https://mega.nz/embed#!';
-    player.src = link + selectedObj.DriveUrl;
-  }
+function infoPage(pathName,information){
+  let selectedObj = '';
+  information.forEach((item,i) => {
+    if (information[i].Name === pathName[0]){
+      selectedObj = information[i];
+    }
+  });
+  console.log(selectedObj);
+  let name = selectedObj.Name;
+  let year = selectedObj.Year;
+  let picture = getElement('id','picture')
+  picture.src = 'images/done/' + convertInput(name,'image') + '.png';
+  let title = getElement('id','title');
+  title.innerText = name + '(' + year + ')';
+  let details = getElement('id','details');
+  details.innerText = selectedObj.Introduction;
+  let player = getElement('id','player');
+  let link = 'https://mega.nz/embed#!';
+  player.src = link + selectedObj.DriveUrl;
 }
--- js/shared.js
+++ js/shared.js
@@ -5,15 +5,25 @@ function initialise(){
   //
   stringProto();
   let pathName = htmlChecker();
-  if (pathName === 'index.html'){
+  if (pathName.indexOf('index') !== -1){
 
   }
-  else if (pathName === 'information_page.html'){
-    readExcel('INFO-PAGE',pathName)
+  else if (pathName.indexOf('information_page') !== -1){
+    let KEY = 'INDEX';
+    let index;
+    if (localStorage[KEY] !== undefined){
+      index = getInfo(KEY);
+    }
+    // index is an array
+    readExcel('INFO-PAGE',index)
   }
-  else if (pathName === 'movies.html' || pathName === 'anime.html'){
-    let KEY = 'FILTER';
-    removeInfo('FILTER');
+  else if (pathName.indexOf('movies') !== -1 || pathName.indexOf('anime') !== -1){
+    let KEY1 = 'FILTER';
+    let KEY2 = 'INDEX';
+    let KEY3 = 'PAGE-BUTTON';
+    removeInfo(KEY1);
+    removeInfo(KEY2);
+    removeInfo(KEY3);
     readExcel('INFORMATION',pathName);
   }
 }
@@ -47,9 +57,21 @@ function readExcel(input,pathName,selOpt,selVal){
     // Call XLSX
     var workbook = XLSX.read(bstr, {type:"binary"});
     // DO SOMETHING WITH workbook HERE
-    var first_sheet_name = workbook.SheetNames[0];
+
     // Get worksheet
-    var worksheet = workbook.Sheets[first_sheet_name];
+    var worksheet = '';
+    workbook.SheetNames.forEach((item) => {
+      if (input !== 'INFO-PAGE'){
+        if (pathName.indexOf(item) !== -1){
+          worksheet = workbook.Sheets[item]
+        }
+      }
+      else{
+        if (pathName[1].indexOf(item) !== -1){
+          worksheet = workbook.Sheets[item]
+        }
+      }
+    });
     var information = XLSX.utils.sheet_to_json(worksheet,{raw:true});
     displayDivs(input,pathName,information,selOpt,selVal);
   }
@@ -61,16 +83,22 @@ function readExcel(input,pathName,selOpt,selVal){
 
 function displayDivs(type,pathName,information,selOpt,selVal){
   if (type === 'INFORMATION'){
-    loadDiv(information,pathName);
+    pageButtonRMB();
+    loadDiv(information,'0');
+    pageDisable();
   }
   else if (type === 'FILTER'){
     sortFuntionality(selOpt,selVal,information);
   }
   else if (type === 'INFO-PAGE'){
-    infoPage(information);
+    infoPage(pathName,information);
+  }
+  else if (type === 'PAGE-BUTTON'){
+    dispContBut(information,selOpt);
   }
 }
 
+// Input is the storage key
 function getInfo(input){
 
   // get storage item
@@ -101,17 +129,63 @@ function setInfo(key,input){
 
 function htmlChecker(){
   let path = window.location.pathname;
-  let pathName = convertInput(path,'pathName');
-  return pathName;
+  return path;
 }
 
-function loadDiv(data,division){
+function pageButtonRMB(action,navigate_Index){
+  let KEY = 'PAGE-BUTTON';
+  if (localStorage[KEY]){
+    let cur_Index = '';
+    if (action === 'Add'){
+      let prev_Index = parseInt(getInfo(KEY));
+      cur_Index = prev_Index + 8;
+    }
+    else if (action === 'Subtract'){
+      let prev_Index = parseInt(getInfo(KEY));
+      cur_Index = prev_Index - 8;
+    }
+    else if (action === 'Navigate') {
+      let selected_Page = parseInt(navigate_Index);
+      let selected_Index = (selected_Page - 1) * 8;
+      cur_Index = selected_Index;
+    }
+    setInfo(KEY,cur_Index);
+  }
+  else{
+    setInfo(KEY,0);
+  }
+}
+
+function getElement(type,name){
+  let output = '';
+  if (type === 'id'){
+    output = document.getElementById(name);
+  }
+  else if (type === 'cn'){
+    output = document.getElementsByClassName(name);
+  }
+  else if (type === 'qs'){
+    output = document.querySelector('.' + name);
+    if (output === null){
+      output = document.querySelector('#' + name);
+    }
+  }
+  return output;
+}
+
+function redirect(input){
+  let KEY = 'INDEX';
+  setInfo(KEY,input);
+  window.location.href = 'information_page.html';
+}
+
+function loadDiv(data,index){
   //
   document.addEventListener('click',(event) => {buttonClicked(event)});
   // get the filter elements
   let type1 = 'filter';
   let filterDivs = getElement('id',type1);
-  let filterList = createFilterList(data,division);
+  let filterList = createFilterList(data);
   let filterOutput = generateContents(type1,filterList);
   filterDivs.innerHTML = filterOutput;
   let filterStorage = sortRMB(filterList[0]);
@@ -119,13 +193,16 @@ function loadDiv(data,division){
   let type2 = 'contents';
   let contentDivs = getElement('id',type2);
   let contentOutput = '';
-  for (let i=0;i<data.length;i++){
-    contentOutput += generateContents(type2,data[i],division);
+  for (let i=index;i<index+8;i++){
+    contentOutput += generateContents(type2,data[i]);
   }
   contentDivs.innerHTML = contentOutput;
+  // record the number of page number
+  let number = data.length;
+  calButNum(type2,number);
 }
 
-function generateContents(type,data,division){
+function generateContents(type,data){
   //
   let output = '';
   if (type === 'filter'){
@@ -145,53 +222,47 @@ function generateContents(type,data,division){
     }
   }
   else if(type === 'contents'){
-    let loweredCase = data.Type.toLowerCase();
-    if (division.indexOf(loweredCase) !== -1){
-      let name = data.Name;
-      output += '<div class="mdl-cell mdl-cell--3-col mdl-card mdl-shadow--4dp video-card">';
-      output += '<div class="mdl-card__media">';
-      output += '<img class="article-image" src="images/done/' + convertInput(name,'image') + '.png" border="0">';
-      output += '</div><div class="mdl-card__supporting-text video-title">';
-      output += '<h2 class="mdl-card__title-text">' + name + '</h2>';
-      output += '</div><div class="mdl-card__actions mdl-card--border">';
-      output += '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect play-button" id="' + name + '">Play</a></div></div>';
-    }
+    let name = data.Name;
+    output += '<div class="mdl-cell mdl-cell--3-col mdl-card mdl-shadow--4dp video-card">';
+    output += '<div class="mdl-card__media">';
+    output += '<img class="article-image" src="images/done/' + convertInput(name,'image') + '.png" border="0">';
+    output += '</div><div class="mdl-card__supporting-text video-title">';
+    output += '<h2 class="mdl-card__title-text">' + name + '</h2>';
+    output += '</div><div class="mdl-card__actions mdl-card--border">';
+    output += '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect play-button" id="' + name + '">Play</a></div></div>';
   }
   return output;
 }
 
-function createFilterList(data,division){
+function createFilterList(data){
   let outputObj = {};
   let key = ['Year','Location','Language','Genre'];
 
   data.forEach((item,i) => {
-    let loweredCase = data[i].Type.toLowerCase();
-    if (division.indexOf(loweredCase) !== -1){
-      for (let i = 0;i<key.length;i++){
-        if (outputObj.hasOwnProperty(key[i])){
-          // current selection
-          let curSel = outputObj[key[i]];
-          // current loop
-          let curLoop = item[key[i]];
-          if (curSel.indexOf(curLoop) === -1){
-            // selected values for current loop
-            let currentValues = convertInput(curLoop);
-            // check if selected values returns in array
-            if (typeof currentValues === 'object' && currentValues.length > 1){
-              for (let i=0;i<currentValues.length;i++){
-                if (curSel.indexOf(currentValues[i]) === -1){
-                  curSel.push(currentValues[i]);
-                }
+    for (let i = 0;i<key.length;i++){
+      if (outputObj.hasOwnProperty(key[i])){
+        // current selection
+        let curSel = outputObj[key[i]];
+        // current loop
+        let curLoop = item[key[i]];
+        if (curSel.indexOf(curLoop) === -1){
+          // selected values for current loop
+          let currentValues = convertInput(curLoop);
+          // check if selected values returns in array
+          if (typeof currentValues === 'object' && currentValues.length > 1){
+            for (let i=0;i<currentValues.length;i++){
+              if (curSel.indexOf(currentValues[i]) === -1){
+                curSel.push(currentValues[i]);
               }
             }
-            else if (typeof currentValues === 'number' || typeof currentValues === 'string'){
-              curSel.push(currentValues);
-            }
+          }
+          else if (typeof currentValues === 'number' || typeof currentValues === 'string'){
+            curSel.push(currentValues);
           }
         }
-        else{
-          outputObj[key[i]] = [];
-        }
+      }
+      else{
+        outputObj[key[i]] = [];
       }
     }
   }
@@ -230,10 +301,6 @@ function convertInput(input,type){
     }
     output = temp;
   }
-  else if (type === 'pathName'){
-    let temp = input.replaceAll('/VidPlayer/','');
-    output = temp;
-  }
   else{
     output = input;
   }
@@ -250,6 +317,7 @@ function buttonClicked(event){
     let pathName = htmlChecker();
     // key to trigger if statement
     let input = 'FILTER';
+    console.log(pathName)
     readExcel(input,pathName,selOpt,selVal)
   }
   else if (event.target.matches('.mdl-button__ripple-container') || event.target.matches('.play-button')){
@@ -257,8 +325,16 @@ function buttonClicked(event){
     if (!selection){
       selection = event.target.id;
     }
-    redirect(selection);
+    let pathName = htmlChecker();
+    redirect([selection,pathName]);
   }
+  else if (event.target.matches('.pages-button')) {
+    // selected button
+    let selBut = event.target.id;
+    checkPageBut(selBut);
+    pageDisable();
+  }
+  //console.log(event.target)
 }
 
 // give hightlights when filter button clicked
@@ -345,11 +421,10 @@ function sortDisplay(input,data){
   let count = input.length;
   let type = 'contents';
   let contentDivs = getElement('id',type);
-  let division = htmlChecker();
   let output = '';
   for (let i=0;i<count;i++){
     let curSel = input[i];
-    output += generateContents(type,data[curSel],division);
+    output += generateContents(type,data[curSel]);
   }
   contentDivs.innerHTML = output;
 }
@@ -360,25 +435,90 @@ function sortFuntionality(type,data,information){
   sortDisplay(indexArray[0],indexArray[1]);
 }
 
-function getElement(type,name){
+// calculate button number
+function calButNum(type,number_of_pg){
   let output = '';
-  if (type === 'id'){
-    output = document.getElementById(name);
+  let pages = getElement('id','pages');
+  if (type === 'filter'){
+
   }
-  else if (type === 'cn'){
-    output = document.getElementsByClassName(name);
-  }
-  else if (type === 'qs'){
-    output = document.querySelector('.' + name);
-    if (output === null){
-      output = document.querySelector('#' + name);
+  else if (type === 'contents'){
+    let still_have = number_of_pg - 8;
+    let count = 2;
+    output += createButDiv('Previous');
+    output += createButDiv('1');
+    if (still_have <= 8){
+      output += createButDiv(count);
     }
+    else if (still_have > 8){
+      let inner_count = count * 8;
+      while (inner_count < still_have){
+        output += createButDiv(count);
+        count += 1;
+        inner_count = count * 8;
+      }
+      if (still_have > 24){
+        output += createButDiv('...');
+      }
+    }
+    output += createButDiv('Next')
   }
+  pages.innerHTML = output;
+}
+
+function createButDiv(count){
+  let output ='<button class="mdl-button mdl-js-button mdl-button--raised pages-button" id="' + count + '">' + count + '</button>';
   return output;
 }
 
-function redirect(input){
-  let KEY = 'INDEX';
-  setInfo(KEY,input);
-  window.location.href = 'information_page.html';
+function pageDisable(){
+  let KEY = 'PAGE-BUTTON';
+  let start_index = getInfo(KEY);
+  let converted_Index = parseInt(start_index/8);
+  let name = 'pages-button';
+  let theClass = getElement('cn',name);
+  for (let i=0;i<theClass.length;i++){
+    if (theClass[i].disabled){
+      theClass[i].disabled = false;
+    }
+    if (theClass[i].id === '...'){
+      theClass[i].disabled = true;
+    }
+  }
+  if (converted_Index < 1){
+    theClass[0].disabled = true;
+    theClass[1].disabled = true;
+  }
+  else{
+    theClass[converted_Index+1].disabled = true;
+  }
+}
+
+function checkPageBut(id){
+  let KEY = 'PAGE-BUTTON';
+  let pathName = htmlChecker();
+  if (id === 'Previous'){
+    pageButtonRMB('Subtract');
+  }
+  else if (id === 'Next'){
+    pageButtonRMB('Add');
+  }
+  else{
+    pageButtonRMB('Navigate',id)
+  }
+  let currentPage = getInfo(KEY);
+  readExcel(KEY,pathName,currentPage)
+}
+
+//display button for selected content
+function dispContBut(information,start_index){
+  let output = '';
+  let type = 'contents';
+  let contentDivs = getElement('id',type);
+  for (let i = start_index;i<start_index+8;i++){
+    if (information[i]!== undefined){
+      output += generateContents(type,information[i]);
+    }
+  }
+  contentDivs.innerHTML = output;
 }
